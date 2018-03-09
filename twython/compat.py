9e563bba3b2fec533437e1a70ee407f47ad6fd4a
andres babino <ababino@gmail.com>
2018-03-09 13:32:00 -0300

    py2 compatiblity. fix problem with utf-8 and url scaped chars

--- twython/compat.py
+++ twython/compat.py
@@ -30,7 +30,19 @@ if is_py2:
     str = unicode
     basestring = basestring
     numeric_types = (int, long, float)
+    def encode_ascii(fun):
+        def inner(x):
+            return fun(x.encode('ASCII'))
+        return inner
+    urlsplit = encode_ascii(urlsplit)
 
+    def decode_utf8(fun):
+        def inner(x):
+            out = fun(x)
+            out = [(k, str(v.decode('utf-8'))) for k, v in out]
+            return out
+        return inner
+    parse_qsl = decode_utf8(parse_qsl)
 
 elif is_py3:
     from urllib.parse import urlencode, quote_plus, parse_qsl, urlsplit
